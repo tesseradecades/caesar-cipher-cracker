965379b27b153babfa18425f9e448da07e187e8c
tesseradecades <nwe8978@rit.edu>
2019-05-27 08:37:55 -0400

    changing to a flask app using vuejs

---
+++ templates/index.vue
@@ -0,0 +1,12 @@
+
+<html>
+    <head>
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    </head>
+    <body>
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    </body>
+</html>
+
